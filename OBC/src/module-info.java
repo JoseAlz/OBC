959cde2fe6ea305efe8b5236b25900fe9f2efc64
JoseAlz <joseufox@hotmail.com>
2022-09-27 16:00:51 -0300

    java github eclipse

---
+++ OBC/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ufox
+ *
+ */
+module OBC {
+}
---
+++ OBC/src/openbootcamp/Hola.java
@@ -0,0 +1,12 @@
+package openbootcamp;
+
+public class Hola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Programacion Java desde Eclipse! ¯\\_(ツ)_/¯");
+		
+
+	}
+
+}
