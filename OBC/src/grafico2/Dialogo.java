7b677f9702eb8bc0946aa901b6178d598e88244f
JoseAlz <joseufox@hotmail.com>
2022-09-29 17:42:17 -0300

    JOptionPane

---
+++ OBC/src/grafico2/Dialogo.java
@@ -0,0 +1,10 @@
+package grafico2;
+
+import javax.swing.JOptionPane;
+
+public class Dialogo {
+	public static void main(String []args) {
+		JOptionPane.showMessageDialog(null, "Bienvenido a Java 2022");
+	}
+
+}
--- OBC/src/module-info.java
+++ OBC/src/module-info.java
@@ -6,4 +6,5 @@
  *
  */
 module OBC {
+	requires java.desktop;
 }
